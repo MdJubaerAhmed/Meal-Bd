415d75145ea3181dc48ef0aa8f67bc13cecadfb3
jubaer ahmed <jubaerahmed2266@gmail.com>
2021-02-11 00:41:24 +0800

    update new code

--- HungryMonster.js
+++ HungryMonster.js
@@ -1,7 +1,3 @@
-const searchBtn = document.getElementById('search_button');
-searchBtn.addEventListener('click', () => {
-    const inputCity = document.getElementById('food').value;
-    getWeatherData(inputFood);
-})
+
 
 
